164d703e5787aaafc8f6eeb1dd188ff68c917b94
Jeremy Immanuel Putra Tandjung <35509768+jeremyimmanuel@users.noreply.github.com>
2019-08-22 13:15:09 -0700

    added comments in constants.java

--- SensorDetectionApp/app/src/main/java/com/example/sensordetection/Constants.java
+++ SensorDetectionApp/app/src/main/java/com/example/sensordetection/Constants.java
@@ -1,9 +1,5 @@
 package com.example.sensordetection;
 
 public class Constants {
-    public static final String SERVER_URL = "http://10.156.4.98:8090/";
+    public static final String SERVER_URL = "http://10.156.4.98:8090/"; //change this to whatever ip address is running the server file
 }
-
-// possible server addresses
-//"http://10.156.4.98:8090/" ~ Donghee's laptop on UW network
-//"http://gcp.anwaraminuddin.com:8090/" ~ Anwar's VM on GCP
