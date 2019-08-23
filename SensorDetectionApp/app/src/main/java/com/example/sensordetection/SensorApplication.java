bf9d82480e6a12ca3db415ad5ed47000ee936478
jeremyimmanuel <jeremy8@uw.edu>
2019-08-23 10:36:03 -0700

    initial commit?

--- SensorDetectionApp/app/src/main/java/com/example/sensordetection/SensorApplication.java
+++ SensorDetectionApp/app/src/main/java/com/example/sensordetection/SensorApplication.java
@@ -7,10 +7,16 @@ import java.net.URISyntaxException;
 
 public class SensorApplication extends Application {
     private Socket mSocket;
+
+
+    public void connectServer(String url)
     {
-        try {
-            mSocket = IO.socket(Constants.SERVER_URL);
-        } catch (URISyntaxException e) {
+        try
+        {
+            mSocket = IO.socket(url);   //old: (Constants.SERVER_URL);
+        }
+        catch (URISyntaxException e)
+        {
             throw new RuntimeException(e);
         }
         mSocket.connect();
