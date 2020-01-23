628902a1e73e6774a74ced296d783e1a33fa7e2f
Jun <jzhenmbp@MacBook-Pro.local>
2020-01-22 17:38:25 -0800

    bhelpb

--- SensorDetectionApp/app/src/main/java/com/example/sensordetection/Constants.java
+++ SensorDetectionApp/app/src/main/java/com/example/sensordetection/Constants.java
@@ -1,5 +1,5 @@
 package com.example.sensordetection;
 
 public class Constants {
-    public static final String SERVER_URL = "http://10.156.9.160:8090/"; //change this to whatever ip address is running the server file
+    public static final String SERVER_URL = "http://10.156.24.128:8090/"; //change this to whatever ip address is running the server file
 }
